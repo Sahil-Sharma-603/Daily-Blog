3bab256fe2b7e95cfbb1d500216329b91da4fe25
Sahil-Sharma-603 <sharmasahil8342@gmail.com>
2025-01-16 23:07:37 -0600

    changes made

--- blog app frontend/js/index.js
+++ blog app frontend/js/index.js
@@ -1,15 +1,43 @@
 
 
-const API_URL = "http://localhost:3000/api/posts";
-const API_BASE_URL = "http://localhost:3000/";
+const API_URL = "http://localhost:8000/api/posts";
+const API_BASE_URL = "http://localhost:8000/";
 
 window.onload = () => {
     getPosts();
 }
 
 const getPosts = () => {
+    fetch(API_URL, {
+        method: 'GET'
+    }).then((response) => {
+        return response.json();
+    }).then((data)=>{
+        buildPosts(data);
+    })
 }
 
 const buildPosts = (blogPosts) => {
-
+    // console.log(blogPosts);
+    let blogPostContent = "";
+    
+    
+    for(post of blogPosts){
+        const postLink = `/post.html?id=${post.id}`; // No spaces around '='
+        const postDate = new Date(parseInt(post.added_date)).toDateString();
+        const postImage = `${API_BASE_URL}${post.post_image}`;
+        blogPostContent += `
+            <a class = "post-link" href = "${postLink}">
+                                    <div class = "post">
+                                        <div class = "post-image" style = "background-image : url(${postImage})"></div>
+                                        <div class = "post-content">
+                                            <div class = "post-date">${postDate}</div>
+                                            <div class = "post-title"><h4>${post.title}</h4></div>
+                                            <div class = "post-text"> ${post.content} </div>
+                                        </div>    
+                                    </div>
+            </a>
+        `
+    }
+    document.querySelector('.blog-post').innerHTML = blogPostContent;
 }
--- blog app frontend/js/post.js
+++ blog app frontend/js/post.js
@@ -4,18 +4,57 @@
  * TODO: Complete buildPost function to fill in the post data in the post.html file using ids
  */
 
-const API_URL = "http://localhost:3000/api/posts/";
-const API_BASE_URL = "http://localhost:3000/";
+const API_URL = "http://localhost:8000/api/posts/";
+const API_BASE_URL = "http://localhost:8000/";
 
 window.onload = () => {
     getPost();
+    // getPostIdParam();
+}
+
+const getPostIdParam = () => {
+    // gives the full link of the url
+    const queryString  = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    // gives the id of the webpage
+    // console.log(urlParams.get("id"));
+    return urlParams.get("id");
 }
 
 const getPost = () => {
     // CODE GOES HERE
+    const postId = getPostIdParam();
+    fetch(`${API_URL}${postId}`,{
+        method: 'GET'
+    }).then((response) => {
+        return response.json();
+    }).then((data) => {
+        buildPost(data);
+    })
 }
 
 const buildPost = (data) => {
-    // HINT: Convert the date number to a Date string 
+
+    console.log(data);
+
+    let postData = "";
+    const postDate = new Date(parseInt(data.added_date)).toDateString();
+    const postImage = `${API_BASE_URL}${data.post_image}`;
+    postData += `
+        <div class = "post-container">
+            <div class = "navigation"><a href="index.html"> Back </a> </div>    
+            <div id = "individual-post-title">${data.title}</div>
+            <div id = "individual-post-date"> ${postDate}</div>
+            <div id = "individual-post-content">${data.content}</div>
+            <div class = "post-image" style = "background-image : url(${postImage})"></div>
+
+        </div>
+
+    `
+
+    document.querySelector('.post-main-container').innerHTML = postData;
+    document.querySelector("header").style.backgroundImage =`url(${postImage})`
+   
+
 }
 
